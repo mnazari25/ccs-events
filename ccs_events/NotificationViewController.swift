bac386c2ab4f1a23345cd97888d5c74cdcbe7025
Mirabutaleb Nazari <mnazari@fullsail.edu>
2016-12-19 14:44:27 -0500

    removed gallery and replaced with notification log

---
+++ ccs_events/NotificationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NotificationViewController.swift
+//  ccs_events
+//
+//  Created by Amir Nazari on 12/13/16.
+//  Copyright © 2016 Amir Nazari. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
