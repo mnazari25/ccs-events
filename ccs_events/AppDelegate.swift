8c3bae4ccd6a5631b38216bee4f76d4a655aabf1
Mirabutaleb Nazari <mnazari@fullsail.edu>
2016-12-19 14:44:57 -0500

    other stuff

--- ccs_events/AppDelegate.swift
+++ ccs_events/AppDelegate.swift
@@ -20,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FIRApp.configure()
+        FIRDatabase.database().persistenceEnabled = true
+        
         
         if #available(iOS 10.0, *) {
             let authOptions : UNAuthorizationOptions = [.alert, .badge, .sound]
